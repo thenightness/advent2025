81c0450baed77a1e67a0e362ef42e971c9aa218c
thenightness <147348310+thenightness@users.noreply.github.com>
2025-12-07 20:20:56 +0100

    day 3 task 2

--- src/day3.rs
+++ src/day3.rs
@@ -3,28 +3,36 @@ pub fn solu(){
     let data: String = std::fs::read_to_string("src/data/day3data").expect("Unable to read file");
     for n in data.lines() {
         let part: Vec<i32> = n.chars().map(|c| c.to_digit(10).unwrap() as i32).collect();
+        let mut summed: String = String::new();
+        let mut values: Vec<[i32; 2]> = Vec::new();
         let mut highest: [i32; 2] = [0, 0];
-        let mut second: [i32; 2] = [0, 0];
-        for n in 0..part.len()-1 {
-            if highest[0] < part[n] {
-                highest[0] = part[n];
-                highest[1] = n as i32;
+        values.push(highest);
+
+        for n in 0..12 {
+            highest = [0, 0];
+            let mut begin: [i32; 2] = values.last().unwrap().clone();
+            if values.len() > 1 {
+                begin[1] += 1;
             }
-            if highest[0] == 9 {
-                break;
+            for i in begin[1] as usize..part.len()-(11 - n) {
+                if highest[0] < part[i] {
+                    highest[0] = part[i];
+                    highest[1] = i as i32;
+                }
+                if highest[0] == 9 {
+                    break;
+                }
             }
+            values.push(highest);
         }
-        for n in highest[1] as usize..part.len() {
-            if second[0] < part[n] && n as i32 != highest[1] {
-                second[0] = part[n];
-                second[1] = n as i32;
-            }
-            if second[0] == 9 {
-                break;
-            }
+        values.remove(0);
+        println!("Values: {:?}", values);
+
+        for v in values {
+            summed.push_str(&v[0].to_string());
         }
-        sum += highest[0] * 10 + second[0]; 
-        println!("{} {}", highest[0], second[0]);
+        println!("Summed: {}", summed);
+        sum += summed.parse::<i128>().unwrap();
     }
     println!("Total sum: {}", sum);
 }
