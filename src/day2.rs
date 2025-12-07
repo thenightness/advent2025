ab1f521092abde7a7f1d0e79f24a5658ee3ba909
thenightness <147348310+thenightness@users.noreply.github.com>
2025-12-07 17:59:05 +0100

    day 2 task 1

--- src/day2.rs
+++ src/day2.rs
@@ -1,3 +1,23 @@
 pub fn solu(){
-    println!("Day 2 solution goes here.");
+    let mut invalid = 0;
+    let data: String = std::fs::read_to_string("src/data/day2data").expect("Unable to read file");
+
+    for n in data.split(',') {
+        let begin = n.split('-').next().unwrap().parse::<i128>().unwrap();
+        let end = n.split('-').nth(1).unwrap().parse::<i128>().unwrap();
+        println!("- - - - - - - - - {} {}", begin, end);
+
+        for i in begin..=end{
+            let count = i.to_string().chars().count();
+            if count % 2 == 0 {
+                let first_half: String = i.to_string().chars().take(count / 2).collect();
+                let second_half: String = i.to_string().chars().skip(count / 2).collect();
+                if first_half == second_half {
+                    println!("Invalid ticket found: {}", i);
+                    invalid += i;
+                }
+            }
+        }
+    }
+    println!("Total invalid sum: {}", invalid);
 }
